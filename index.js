f4769e5e664d1b02aa11260638592017b90801de
Precious <preciousmaregwa1997@gmail.com>
2022-04-06 15:19:12 +0300

    add changes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1000 + 1)
+
+let num3 = 84
+let num4 = 5
+let mod = num3 % num4;
+
+let max=Math.max(1,4,8,15,20);
